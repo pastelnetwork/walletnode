ed0d0d0c7331e4f1386f6e247268f9d55d6a2905
YurGas <gasparyan.yur@gmail.com>
2021-04-20 19:44:29 +0300

    Merged with master

---
+++ storage/config.go
@@ -0,0 +1,14 @@
+package storage
+
+const (
+	chatDBDir = "chat"
+)
+
+type Config struct {
+	ChatDBDir string
+}
+
+// NewConfig returns a new Config instance.
+func NewConfig() *Config {
+	return &Config{ChatDBDir: chatDBDir}
+}
